bca866fb840d8b14af4e226c967a53441994c53b
leviyukenn <yokunu_yjy@yahoo.co.jp>
2022-03-26 10:21:13 +0000

    add enviroment variables

--- src/const/const.tsx
+++ src/const/const.tsx
@@ -1,6 +1,6 @@
-export const GRAPHQL_SERVER_URL = "http://localhost:3000/graphql";
-export const SERVER_URL = "http://localhost:3000";
-export const FRONTEND_URL = "http://localhost:3005";
+export const GRAPHQL_SERVER_URL = process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL;
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+export const FRONTEND_URL = "";
 
 export const GOOGLE_AUTH_CLIENT_ID =
   "652178129477-fj9en563gfpb0fr7l9s1llk1f5gv017k.apps.googleusercontent.com";
