de8edd5b587fb28eca293c1dd3c630f6d5d1d10e
leviyu <yokunu_yjy@yahoo.co.jp>
2022-03-31 07:39:07 +0900

    fixed a bug that browser can't get the google client id

--- src/const/const.ts
+++ src/const/const.ts
@@ -2,4 +2,4 @@ export const GRAPHQL_SERVER_URL =
   process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL || "";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "";
 export const FRONTEND_URL = "";
-export const GOOGLE_AUTH_CLIENT_ID = process.env.GOOGLE_AUTH_CLIENT_ID || "";
+export const GOOGLE_AUTH_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID || "";
