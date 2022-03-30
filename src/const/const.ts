332cd3e87c57cd9867fac45264e190e50043855f
leviyu <yokunu_yjy@yahoo.co.jp>
2022-03-30 19:48:30 +0900

    remove env files

--- src/const/const.ts
+++ src/const/const.ts
@@ -2,6 +2,4 @@ export const GRAPHQL_SERVER_URL =
   process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL || "";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "";
 export const FRONTEND_URL = "";
-
-export const GOOGLE_AUTH_CLIENT_ID =
-  "652178129477-fj9en563gfpb0fr7l9s1llk1f5gv017k.apps.googleusercontent.com";
+export const GOOGLE_AUTH_CLIENT_ID = process.env.GOOGLE_AUTH_CLIENT_ID || "";
